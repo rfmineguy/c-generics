d889adc5357cdaee08b4cb1e950e0ba15c0faa86
rfmineguy <rfmineguy@gmail.com>
2024-05-14 11:45:24 -0700

    put, get, free working well

--- chash_tplt.h
+++ chash_tplt.h
@@ -8,7 +8,7 @@
 
 #define HT_STRUCT_DEF(key_name, value_name, key_type, value_type)\
 typedef struct {\
-    struct JOIN3(ht_, value_name, _ll)* buckets[10]; \
+    struct JOIN3(ht_, value_name, _ll)* buckets[BUCKET_COUNT]; \
 } HT_MAIN_STRUCT_NAME(key_name, value_name);
 
 #define HT_NODE_DEF(key_name, value_name, key_type, value_type)\
@@ -22,18 +22,70 @@ typedef struct HT_LL_STRUCT_NAME(key_name, value_name) {\
 	HT_NODE_DEF(key_name, value_name, key_type, value_type)\
 	HT_STRUCT_DEF(key_name, value_name, key_type, value_type)\
 
-#define HT_PROTOTYPES(key_name, value_name, key_type, value_type)\
-	void       JOIN5(ht_, key_name, _, value_name, _put)(HT_MAIN_STRUCT_NAME(key_name, value_name)*, key_type, value_type);\
-	value_type JOIN5(ht_, key_name, _, value_name, _get)(key_type);\
-	int        JOIN5(ht_, key_name, _, value_name, _hash)(key_type key);
+#define HT_PUT_PROTO(key_name, value_name, key_type, value_type)\
+	void JOIN5(ht_, key_name, _, value_name, _put)(HT_MAIN_STRUCT_NAME(key_name, value_name)*, key_type, value_type)
 
-#define HT_HASH_IMPL(key_name, value_name, key_type)\
-	int JOIN5(ht_, key_name, _, value_name, _hash)(key_type key)
+#define HT_GET_PROTO(key_name, value_name, key_type, value_type)\
+	value_type JOIN5(ht_, key_name, _, value_name, _get)(HT_MAIN_STRUCT_NAME(key_name, value_name)*, key_type)
 
-#define HT_IMPL(key_name, value_name, key_type, value_type)\
-	void JOIN5(ht_, key_name, _, value_name, _put)(JOIN4(ht_, key_name, _, value_name) *ht, key_type key, value_type val) {\
+#define HT_HASH_PROTO(key_name, value_name, key_type, value_type)\
+	int JOIN5(ht_, key_name, _, value_name, _hash)(key_type)\
+
+#define HT_FREE_PROTO(key_name, value_name, key_type, value_type)\
+	void JOIN5(ht_, key_name, _, value_name, _free)(HT_MAIN_STRUCT_NAME(key_name, value_name)*)\
+
+#define HT_CMP_PROTO(key_name, value_name, key_type, value_type)\
+	bool JOIN5(ht_, key_name, _, value_name, _cmpdef)(key_type, key_type)\
+
+#define HT_PUT_IMPL(key_name, value_name, key_type, value_type)\
+	void JOIN5(ht_, key_name, _, value_name, _put)(HT_MAIN_STRUCT_NAME(key_name, value_name)* ht, key_type key, value_type val) {\
 		int hash = JOIN5(ht_, key_name, _, value_name, _hash)(key);\
 		if (ht->buckets[hash] == NULL) {\
-			ht->buckets[hash] = calloc(1, sizeof(ht->buckets[0]));\
+			ht->buckets[hash] = calloc(1, sizeof(HT_LL_STRUCT_NAME(key_name, value_name)));\
+			ht->buckets[hash]->val = val;\
+			ht->buckets[hash]->key = key;\
+			return;\
+		}\
+		else {\
+			HT_LL_STRUCT_NAME(key_name, value_name)* node = ht->buckets[hash];\
+			while (node->next) node = node->next;\
+			node = calloc(1, sizeof(HT_LL_STRUCT_NAME(key_name, value_name)));\
+			node->val = val;\
+			node->key = key;\
 		}\
 	}\
+
+#define HT_GET_IMPL(key_name, value_name, key_type, value_type, default)\
+	value_type JOIN5(ht_, key_name, _, value_name, _get)(HT_MAIN_STRUCT_NAME(key_name, value_name)* ht, key_type key) {\
+		int hash = JOIN5(ht_, key_name, _, value_name, _hash)(key);\
+		if (ht->buckets[hash] == NULL) {\
+			return default;\
+		}\
+		else if (JOIN5(ht_, key_name, _, value_name, _cmpdef)(ht->buckets[hash]->key, key)){\
+			return ht->buckets[hash]->val;\
+		}\
+		else {\
+			HT_LL_STRUCT_NAME(key_name, value_name)* node = ht->buckets[hash];\
+			while (node && JOIN5(ht_, key_name, _, value_name, _cmpdef)(node->key, key)) node = node->next;\
+			if (!node) return default;\
+			return node->val;\
+		}\
+	}\
+
+#define HT_HASH_DEF(key_name, value_name, key_type, value_type)\
+	int JOIN5(ht_, key_name, _, value_name, _hash)(key_type key)
+
+#define HT_FREE_IMPL(key_name, value_name, key_type, value_type)\
+	void JOIN5(ht_, key_name, _, value_name, _free)(HT_MAIN_STRUCT_NAME(key_name, value_name)* ht) {\
+		for (int i = 0; i < BUCKET_COUNT; i++) {\
+			HT_LL_STRUCT_NAME(key_name, value_name) *n = ht->buckets[i];\
+			while (n) {\
+				HT_LL_STRUCT_NAME(key_name, value_name) *t = ht->buckets[i];\
+				n = n->next;\
+				free(t);\
+			}\
+		}\
+	}\
+
+#define HT_CMP_DEF(key_name, value_name, key_type, value_type)\
+	bool JOIN5(ht_, key_name, _, value_name, _cmpdef)(key_type key1, key_type key2)
--- main.c
+++ main.c
@@ -1,19 +1,40 @@
 #include "chash_tplt.h"
 #include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#define BUCKET_COUNT 10
 HT_DEF(str, int, const char*, int);
-HT_PROTOTYPES(str, int, const char*, int);
-HT_HASH_IMPL(str, int, const char*) {
-	int hash = 0;
-	for (int i = 0; i < strlen(key); i++) {
-		hash += key[i] * 3402;
-	}
-	return hash % 10;
+HT_PUT_PROTO(str, int, const char*, int);
+HT_GET_PROTO(str, int, const char*, int);
+HT_HASH_PROTO(str, int, const char*, int);
+HT_FREE_PROTO(str, int, const char*, int);
+HT_CMP_PROTO(str, int, const char*, int);
+
+HT_CMP_DEF(str, int, const char*, int) {
+	return strcmp(key1, key2) == 0;
 }
-HT_IMPL(str, int, const char*, int);
+HT_HASH_DEF(str, int, const char*, int) {
+	unsigned long hash = 5381;
+	int c;
+
+	while ((c = *key++))
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+	return hash % BUCKET_COUNT;
+}
+HT_GET_IMPL(str, int, const char*, int, -1)
+HT_PUT_IMPL(str, int, const char*, int)
+HT_FREE_IMPL(str, int, const char*, int)
 
 int main() {
 	ht_str_int ht = {0};
-	ht_str_int_put(&ht, "test", 1);
-	int v = ht_str_int_get("test");
+	ht_str_int_put(&ht, "test", 9);
+	int v;
+	if ((v = ht_str_int_get(&ht, "test"))) {
+		printf("Found test: %d\n", v);
+	}
+
+	ht_str_int_free(&ht);
 }
